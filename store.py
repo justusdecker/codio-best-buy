d52944473ad1fb63fb5a8b2d7b874c6a51b82672
Justus <justus.d2025@gmail.com>
2025-06-06 18:15:18 +0200

    added: removing products & ordering

--- store.py
+++ store.py
@@ -8,11 +8,11 @@ class Store:
             Product("Bose QuietComfort Earbuds", price=250, quantity=500),
             Product("MacBook Air M2", price=1450, quantity=100),
             Product("A Big Tasty Bacon🍔", price=250, quantity=500)] #I'm hungry 🤤
-    def remove_product(self):
-        pass
+    def remove_product(self,product: Product):
+        self.products.remove(product)
     def get_total_quantity(self) -> int:
         return sum([i.get_quantity() for i in self.products])
     def get_all_products(self) -> int:
         return sum([i.is_active() for i in self.products])
-    def order(self):
-        pass
+    def order(self, product: tuple[Product, int]):
+        return sum([prod.buy(quan) for prod, quan in product])
