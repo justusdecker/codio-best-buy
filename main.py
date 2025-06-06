0146e3fee2abd870aa3e18b8381ad375747f5d6e
Justus <justus.d2025@gmail.com>
2025-06-06 18:26:55 +0200

    added: quit, list of products & total quantity

--- main.py
+++ main.py
@@ -10,7 +10,23 @@ Welcome to BestBuy!
 """
 
 def main():
-    print(MENU)
+    is_running = True
+    while is_running:
+        print(MENU)
+        user_input = input('Please choose a number: ')
+        match user_input:
+            case "1":
+                print(f"{'Name':<30} {'Price':<6} {'Quantity':<15}")
+                for prod in STORE.products:
+                    print(prod.show())
+                input('ENTER to continue!')
+            case "2":
+                print(STORE.get_total_quantity())
+                input('ENTER to continue!')
+            case "3":
+                pass
+            case "4":
+                is_running = False
 
 if __name__ == '__main__':
     main()
