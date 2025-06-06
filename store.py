56db3815b07ada68a5bd1694f77135090423a136
Justus <justus.d2025@gmail.com>
2025-06-06 19:06:58 +0200

    added docstrings

--- store.py
+++ store.py
@@ -9,10 +9,14 @@ class Store:
             Product("MacBook Air M2", price=1450, quantity=100),
             Product("A Big Tasty Bacon", price=250, quantity=500)] #I'm hungry 🤤
     def remove_product(self,product: Product):
+        """ Remove a given Product from ``products`` """
         self.products.remove(product)
     def get_total_quantity(self) -> int:
+        """ Get all products quantity """
         return sum([i.get_quantity() for i in self.products])
     def get_all_products(self) -> int:
+        """ Get all active products """
         return sum([i.is_active() for i in self.products])
     def order(self, product: tuple[Product, int]):
+        """ Take an order by calling for each element in product the buy method of a Product Instance! """
         return sum([prod.buy(quan) for prod, quan in product])
